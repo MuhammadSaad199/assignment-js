1ea9dab7e2e192a4a0ad56a90b496086cafe95b9
M.Saad <mohammadsaadjavediqbal1999@gmail.com>
2020-06-25 23:53:03 +0500

    assignment 6

---
+++ app.js
@@ -0,0 +1,355 @@
+//   21 to 25 chapter
+
+
+1// var first = prompt("Enter First Name");
+// var last = prompt("Enter Last Name");
+// var fullname = (first +" "+last+ "! How Are You")
+// document.write(fullname);
+
+
+// var a = prompt("Enter Favourite Mobile Name")
+// if(a === "nokia"){
+//     console.log("Mobile phone are very solid.");
+// }else if(a === "samsung"){
+//     console.log("Mobile phone are very Low Quality.");
+// }
+// var indxNum = a.indexOf("solid") 
+// console.log(firsttext)
+
+
+
+3// var a = "Pakistani"
+// document.write(a.charAt(7))
+
+
+4// var a = "Hello World"
+// var b = a.lastIndexOf("l")
+// document.write(b);
+
+
+5// var a = "Pakistani"
+// document.write(a.charAt("3"));
+
+
+// var first = prompt("Enter First Name");
+// var last = prompt("Enter Last Name");
+// var fullname = (first +" "+last+ "! How Are You")
+// document.write(fullname);
+
+
+7// var a = "hyderabad";
+// var b = a.replace("hyder" , "Islam")
+// document.write(b)
+
+
+8// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var b =  message.replace(/and/g ,"&")
+// document.write(b)
+
+
+9// Number =("472")
+// console.log(Number)
+
+
+10// var a = prompt("Enter Any Word")
+// var a = a.toUpperCase()
+// document.write(a)
+
+
+11// var a = prompt("Enter Any Word")
+// var b = a.slice(0,1);
+// var c = b.toUpperCase()
+// var d = a.slice(1)
+// var e = d.toLowerCase()
+// document.write(c+e)
+
+
+12// var a = 35.86
+// var b = a.toFixed(2)
+// var c = b.replace("." , "")
+// console.log(c.toString())
+
+
+// var a = prompt("Enter the username" , "Enter atleast six letter")
+// var b = a.slice()
+// if (a === "@" || a === "!" || a === "." || a === ","){
+// alert("Thank You")
+// }
+
+
+// var a = prompt("Entered Password")
+// var b = 
+
+
+// var a = "Karachi Of University"
+// var b = a.split(0, 1)
+// console.log(b)
+
+
+var a = prompt("enter the value");
+var b = a.lastIndexOf("")
+document.write(a.charAt(""+b))
+
+
+
+// var a = "The quick brown fox jumps over the lazy dog"
+// var b = a("the")
+// document.write(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   26 to 30 chapter
+
+
+
+
+
+1// var a = "5.3565"
+// document.write("Number: "+a +"<br>")
+// var b = Math.round(a)
+// document.write("Round off value: " + b +"<br>")
+// var c = Math.floor(a)
+// document.write("floor value: " + c +"<br>")
+// var d = Math.ceil(a)
+// document.write("ceil value: " +   d)
+
+
+2// var a = "-8.3565"
+// document.write("Number: "+a +"<br>")
+// var b = Math.round(a)
+// document.write("Round off value: " + b +"<br>")
+// var c = Math.floor(a)
+// document.write("floor value: " + c +"<br>")
+// var d = Math.ceil(a)
+// document.write("ceil value: " +   d)
+
+
+
+
+
+8// var a = prompt("Enter the number" , "Guess And Win");
+// if(a == 7){
+//     document.write("Bingo! You Win")
+// }
+// else if(a == 6 || a == 8){
+//     document.write("Close Enough! to the answer")
+// }
+// else if(a < 1 || a > 10 ){
+//     document.write("Enter 1 to 10 number And Win Exciting Prizes")
+// }
+// else{
+//     document.write("Try Again")
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//   31 to 34 chapter
+
+
+
+
+1// var a = new Date();
+// document.write(a);
+
+
+2// var monthnames = ["January" , "Febuary" , "March" , "April" , "May" , "June" , "July" , "August" , "September" , "October" , "November" , "December"];
+// var now = new Date();
+// var themonth = now.getMonth();
+// var names = monthnames[themonth]
+// alert("Current Month: " + names);
+
+
+3//  var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//  var now = new Date();
+//  var theDay = now.getDay();
+//  var nameOfToday = dayNames[theDay];
+//  document.write("Today is " + nameOfToday)
+
+
+4//  var dayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+//  var now = new Date();
+//  var theDay = now.getDay();
+//  var nameOfToday = dayNames[theDay];
+//  if (nameOfToday === "Sun" || nameOfToday === "Sat"){
+//     alert( "Its Fun Day")
+//  }
+//  else if (alert("Its Working Day"));
+
+
+// var Current = new Date();
+// var date = Current.getDate();
+// if (date === 5){
+//     alert("first")
+// }else if(alert("seca"));
+
+// "1" || "2" || "3" || "4" || "5" || "6" || "7" || "8" || "9" || "10" || "11" || "12" || "13" || "14" || "15"
+// "16" || "17" || "18" || "19" || "20" || "21" || "22" || "23" || "24" || "25" || "26" || "27" || "28" || "29" || "30" || "31" 
+
+
+6// var a = new Date();
+// var b = a.getTime();
+// //var c = b/1000*60*60
+// document.write(a + "<br>" + b);
+
+
+var a = 
+
+
+8// var d = new Date();
+// var a = d.setMonth("5");
+// var b = d.setDate("30");
+// var c = d.setFullYear("2020");
+// document.write("Later Date: " + d)
+
+
+
+13// var a = new Date(prompt("Enter Your Birth  Year"))
+
+// var amili = a.getTime();
+// var today = new Date();
+// var todaymili = today.getTime();
+// var diff = todaymili - amili;
+// var accuage = Math.floor(diff/(1000*60*60*24*30*12))
+// document.write(accuage);
+
+
+
+14//  var a = (prompt("Enter Your Name"))
+//  var a1 = a.slice(0,1);
+//  var b = a.slice(1)
+//  a1= a1.toUpperCase();
+//  b = b.toLowerCase();
+//  var b = (prompt("Enter Your Month"))
+//  var c = (prompt("Enter Your Number Of Units"))
+//  var d = (prompt("Enter Your Charges Per Amount Unit"))
+ 
+//  var e = c*d;
+//  var f = e+350
+
+//  document.write("K-Electeic Bill" + "<br>" + "Customer Name: " + a1+b + "<br>"  + "Month: " + b + "<br>"  + "Number of units: " + c + "<br>"  + "<br>" + 
+//  "Charges Per Unit: " + d + "<br>" + "Net Amount Payable(within Due Date): " + e + "<br>" + "Late payment surcharge: 350"  + "<br>" + 
+//  "Gross Amount Payable (after Due Date): " + f)
+
+
+
+
+
+
+
+    // 35 to 38 chapter
+
+
+
+1//     function date() {
+//        var a = new Date();
+//        document.write(a);
+//         }
+// date ();
+
+
+2// function greet(){
+//     var name1 = "Muhammad";
+//     var name2 = "Saad";
+//  document.write(name1 +" "+name2)
+// }
+// greet();
+
+
+3// function sum(a , b){
+// return a + b
+// }
+// document.write(sum(3,7))
+
+
+4// function calc (value1 , opr , value2) {
+//     if (opr === "+"){
+//         return value1 + value2}
+// else if (opr === "-"){
+//     return value1 - value2}
+// else if (opr === "*"){
+//     return value1 * value2}
+// else if (opr === "/"){
+//     return value1 / value2}
+//     else{
+//         return "Please Enter Correct Operator"}
+//     }
+// document.write(4 - 8)
+
+
+7// var a = prompt("Start Value Of Counting")
+// var b = prompt("End Value Of Counting")
+
+// function loop(){
+//     for (b = a; b <= b; ++b)
+// return b
+// }
+// document.write(b);
+
+
+9// function rect(a , b){
+// document.write(a * b)
+// }
+// rect(8 , 4)
+
+// function rect(a=8,b=4){
+//     document.write(a*b)
+// }
+// rect();
+
+
+10// var word = prompt("Enter Your Word")
+// var b = "";
+// function palin(){
+//     for(var i = word.length - 1; i >= 0; i--){
+//         b += word[i]
+//     }
+//     if(word === b){
+//         document.write(word + " is palindrome word")
+//        }
+//     }
+//        palin(); 
+
+
+
+11// var a = (prompt("Enter Your Name"))
+// var b = a.slice(0,1);
+// var c= b.toUpperCase();
+// var d = c+a
+// document.write(d)
+
+
+14// var r = 7;
+// function radius(){
+//     document.write("Circumference Of Circle " + 2*3.14*r)
+// }
+// radius();
+
+// var a = 4;
+// function area(){
+//     document.write("Area Of Circle " + 3.14*a*a)
+// }
+// area();
